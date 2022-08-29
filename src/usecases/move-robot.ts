b655e11c2765e02b6ac9e51e7f2293118f31574e
Andre Ravazzi <amravazzi@gmail.com>
2022-08-29 19:23:55 -0300

    feat: update possible usecases

--- src/usecases/move-robot.ts
+++ src/usecases/move-robot.ts
@@ -1,14 +1,13 @@
-
-import { Robot } from '../domain/Robot';
-import { Planet } from '../domain/Planets/Planet';
-import { Input } from '../domain/Input';
+import { Robot } from "../domain/Robot";
+import { Planet } from "../domain/Planets/Planet";
+import { Input } from "../domain/Input";
 import { Coordinate } from "../domain/Coordinate";
-// what's needed? Robot, Planet/Mars, Coordinate, Orientation, Input, Command
+
+// what's needed? Robot, Planet/Mars, Coordinate, Orientation, Input, Commands
 export class MoveRobot {
+  constructor(robot: Robot, planet: Planet, input: Input) {}
 
-  constructor(robot: Robot, planet: Planet, input: Input){}
-
-  move() {
+  moveRobot() {
 
   }
 }
