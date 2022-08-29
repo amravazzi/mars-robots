5b64329e7aa6bbc69636c7dc74ccb45743476710
Andre Ravazzi <amravazzi@gmail.com>
2022-08-29 00:36:50 -0300

    feat: add moving robot usecase class

--- src/usecases/move-robot.ts
+++ src/usecases/move-robot.ts
@@ -1,5 +1,14 @@
 
+import { Robot } from '../domain/Robot';
+import { Planet } from '../domain/Planets/Planet';
+import { Input } from '../domain/Input';
+import { Coordinate } from "../domain/Coordinate";
 // what's needed? Robot, Planet/Mars, Coordinate, Orientation, Input, Command
 export class MoveRobot {
 
+  constructor(robot: Robot, planet: Planet, input: Input){}
+
+  move() {
+
+  }
 }
