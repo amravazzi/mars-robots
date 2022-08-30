16073cf7252bea67588e8ece188b90f14002ca18
Andre Ravazzi <amravazzi@gmail.com>
2022-08-30 12:41:27 -0300

    feat: more guards on coordinate

--- src/domain/Coordinate.ts
+++ src/domain/Coordinate.ts
@@ -1,10 +1,10 @@
 // export type Coordinate = [string, string];
 export class Coordinate {
-  x: number;
-  y: number;
+  x: number | undefined;
+  y: number | undefined;
 
-  constructor(x: number, y: number) {
-    this.x = x;
-    this.y = y;
+  constructor(x: number | undefined, y: number | undefined) {
+    this.x = typeof x === undefined ? 0 : x;
+    this.y = typeof y === undefined ? 0 : y;
   }
 }
