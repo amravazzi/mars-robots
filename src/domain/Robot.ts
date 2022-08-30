ba2d5d30e5f674ab5217208e0e948939ada2055f
Andre Ravazzi <amravazzi@gmail.com>
2022-08-30 12:41:58 -0300

    feat: add more robot properties

--- src/domain/Robot.ts
+++ src/domain/Robot.ts
@@ -1,18 +1,28 @@
 import { Orientation } from "../domain/Orientation";
 import { Coordinate } from "../domain/Coordinate";
 import { Planet } from "./Planets/Planet";
+// import { Command } from "./Commands/Command";
 
 // IsOutOfBounds
 
 export class Robot {
   private readonly planet: Planet;
   public currentOrientation: Orientation;
+  public currentCoordinate: Coordinate;
   public isLost: boolean;
+  public commands: string[];
 
-  constructor(planet: Planet, currentOrientation: Orientation) {
+  constructor(
+    planet: Planet,
+    currentOrientation: Orientation,
+    currentCoordinate: Coordinate,
+    commands: string[]
+  ) {
     this.planet = planet;
     this.currentOrientation = currentOrientation;
+    this.currentCoordinate = currentCoordinate;
     this.isLost = false;
+    this.commands = commands;
   }
 
   shouldMoveForward(nextCoordinate: Coordinate) {
