dd644a47a7bde5bd2f18ad33f1c2463bcff6668d
Andre Ravazzi <amravazzi@gmail.com>
2022-08-30 09:05:19 -0300

    feat: only the first 100 commands will be read

--- src/domain/Input.ts
+++ src/domain/Input.ts
@@ -34,19 +34,29 @@ export class Input {
         if (!el) {
           const [startingPosX, startingPosY, startingDirection] =
             robotsRaw[i - 2].split(" ");
+          const commandChain: string[] = robotsRaw[i - 1].split("");
+
+          if (commandChain.length > 100)
+            console.warn("Only the first 100 commands will be executed");
+
           return {
             startingPosition: [parseInt(startingPosX), parseInt(startingPosY)],
             startingDirection,
-            commands: robotsRaw[i - 1].split(""),
+            commands: commandChain.slice(0, 99),
           };
         }
         if (robotsRaw.length - 1 === i) {
           const [startingPosX, startingPosY, startingDirection] =
             robotsRaw[i - 1].split(" ");
+          const commandChain: string[] = robotsRaw[i].split("");
+
+          if (commandChain.length > 100)
+            console.warn("Only the first 100 commands will be executed");
+
           return {
             startingPosition: [parseInt(startingPosX), parseInt(startingPosY)],
             startingDirection,
-            commands: robotsRaw[i].split(""),
+            commands: commandChain.slice(0, 99),
           };
         }
       })
