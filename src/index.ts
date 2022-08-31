0b49cfb7ce8eebcca911b0471f1a6032f3aba6c1
Andre Ravazzi <amravazzi@gmail.com>
2022-08-31 09:59:31 -0300

    feat: call the last usecase

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Input } from "./domain/Input";
 import { Orientation } from "./domain/Orientation";
 import { Mars } from "./domain/Planets/Mars";
 import { FileInput } from "./infra/FileInput";
+import { MoveRobotsSwarm } from "./usecases/move-robots-swarm";
 import { StartRobots } from "./usecases/start-robots";
 
 // bootstrap
@@ -12,19 +13,16 @@ import { StartRobots } from "./usecases/start-robots";
   const input = new Input(telemetryRaw);
   const telemetry = input.getData();
 
-  console.log({ a: telemetry.robotsProperties[2] });
-
   const mars = new Mars(
     new Coordinate(telemetry.planetBoundaries[0], telemetry.planetBoundaries[1])
   );
 
   const robots = new StartRobots(mars, telemetry);
 
-  console.log({ StartRobots: robots.start() });
-  console.log({ telemetry });
-  // console.log({ robotsProperties: telemetry.robotsProperties });
+  const moveRobots = new MoveRobotsSwarm(robots.start(), mars);
+  const robotsAfter = moveRobots.move();
   // console.log({
-  //   robotsProperties:
-  //     typeof Orientation,
+  //   a: robotsAfter[2].currentCoordinate,
+  //   b: Orientation[robotsAfter[2].currentOrientation],
   // });
 })();
