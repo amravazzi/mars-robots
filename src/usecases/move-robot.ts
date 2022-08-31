e708cd9a0a7e140fda23d9d77ce81e845fe3b166
Andre Ravazzi <amravazzi@gmail.com>
2022-08-31 02:22:26 -0300

    feat: add movement cases

--- src/usecases/move-robot.ts
+++ src/usecases/move-robot.ts
@@ -3,6 +3,9 @@ import { Planet } from "../domain/Planets/Planet";
 import { Coordinate } from "../domain/Coordinate";
 import { Orientation } from "../domain/Orientation";
 import { Move } from "./move";
+import { MoveForward } from "../domain/Commands/MoveForward";
+import { TurnLeft } from "../domain/Commands/TurnLeft";
+import { TurnRight } from "../domain/Commands/TurnRight";
 
 export class MoveRobot implements Move {
   public robot;
@@ -13,21 +16,51 @@ export class MoveRobot implements Move {
     this.planet = planet;
   }
 
-  move() /*: Robot*/ {
+  move(): Robot {
     const commands = this.robot.commands;
-    let finalPosition: Coordinate;
-    let finalOrientation: Orientation;
+    let intermediateCoordinate: Coordinate = this.robot.currentCoordinate;
+    let intermediateOrientation: Orientation = this.robot.currentOrientation;
+
+    console.log("---------------");
 
     for (let comm of commands) {
+      if (this.robot.isLost) break;
+
       switch (comm) {
         case "F":
+          if (this.planet.isCoordinateScented(intermediateCoordinate)) break;
+          const mf = new MoveForward(
+            intermediateCoordinate,
+            intermediateOrientation
+          );
+          intermediateCoordinate = mf.exec();
+          if (this.planet.isCoordinateOutOfBoundaries(intermediateCoordinate)) {
+            this.robot.isLost = true;
+            this.planet.createScentedCoordinate(intermediateCoordinate);
+          }
+          break;
+
+        case "L":
+          const tl = new TurnLeft(intermediateOrientation);
+          intermediateOrientation = tl.exec();
+          break;
+
+        case "R":
+          const tr = new TurnRight(intermediateOrientation);
+          intermediateOrientation = tr.exec();
           break;
 
         default:
+          console.warn(`Command ${comm} not recognized or not available.`);
           break;
       }
     }
 
-    return 0;
+    this.robot.currentCoordinate = intermediateCoordinate;
+    this.robot.currentOrientation = intermediateOrientation;
+
+    console.log({ intermediateCoordinate, intermediateOrientation });
+
+    return this.robot;
   }
 }
