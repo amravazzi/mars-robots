f497053feaf6e5fccbf706e4cbbd53dfc0699919
Andre Ravazzi <amravazzi@gmail.com>
2022-08-31 18:25:29 -0300

    ref: refactor application entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,12 @@
-import { Coordinate } from "./domain/Coordinate";
-import { Input } from "./domain/Input";
-import { Orientation } from "./domain/Orientation";
-import { Mars } from "./domain/Planets/Mars";
-import { FileInput } from "./infra/FileInput";
-import { MoveRobotsSwarm } from "./usecases/move-robots-swarm";
-import { StartRobots } from "./usecases/start-robots";
+import { makeRobotsSwarmMove } from "./main/factories/robots";
+import { makeTelemetry } from "./main/factories/telemetry";
+import { ConsoleOutput } from "./infra/IO/Outputs/ConsoleOutput";
+import { makeRobotsReport } from "./main/factories/report";
 
 // bootstrap
 (async function () {
-  const file = new FileInput("./src/telemetry.txt");
-  const telemetryRaw = await file.getTelemetry();
-  const input = new Input(telemetryRaw);
-  const telemetry = input.getData();
-
-  const mars = new Mars(
-    new Coordinate(telemetry.planetBoundaries[0], telemetry.planetBoundaries[1])
-  );
-
-  const robots = new StartRobots(mars, telemetry);
-
-  const moveRobots = new MoveRobotsSwarm(robots.start(), mars);
-  const robotsAfter = moveRobots.move();
+  const telemetry = await makeTelemetry();
+  const movedSwarm = makeRobotsSwarmMove(telemetry);
+  const printer = new ConsoleOutput();
+  makeRobotsReport(movedSwarm, printer);
 })();
