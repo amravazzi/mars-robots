567d5313122a13b4adf01b0ce562c3b8a6c85d06
Andre Ravazzi <amravazzi@gmail.com>
2022-08-31 02:21:34 -0300

    ref: remove undefined in union type

--- src/domain/Coordinate.ts
+++ src/domain/Coordinate.ts
@@ -1,10 +1,10 @@
 // export type Coordinate = [string, string];
 export class Coordinate {
-  x: number | undefined;
-  y: number | undefined;
+  x: number;
+  y: number;
 
-  constructor(x: number | undefined, y: number | undefined) {
-    this.x = typeof x === undefined ? 0 : x;
-    this.y = typeof y === undefined ? 0 : y;
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
   }
 }
